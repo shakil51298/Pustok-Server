dc0bb09ab2c0fd934fab3a26ca41b272c98db75d
Md Khalid Hossain <zikaiwen02@gmail.com>
2021-04-19 01:00:04 +0800

    problem

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bptoi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const app = express()
-const port = 5000 || process.env.PORT
+const port = process.env.PORT || 5000 
 app.use(bodyParser.json())
 app.use(cors());
 
